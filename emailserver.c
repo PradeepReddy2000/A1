0bb65b7d142d6e083beab15eae0eca7b5cd4592d
kayalanithinsai <nithinkayala@gmail.com>
2021-03-04 00:40:53 +0530

    Getting COre dumped from LSTU function test it once.

--- emailserver.c
+++ emailserver.c
@@ -5,15 +5,41 @@
 #include <string.h> 
 #include <sys/socket.h> 
 #include <sys/types.h> 
+#include <dirent.h>
 #define MAX 80 
 #define PORT 8080 
 #define SA struct sockaddr 
 
-
-char* command_processor()
-{
-			
+void LSTU(){
+	DIR *d;
+	struct dirent *dir;
+	d = opendir(".");
+	if(d){
+		while(dir = readdir(d) != NULL){
+			// if(strncmp("emailserver.c",dir->d_name,13) == 0){
+				// printf()
+			// }
+			// if(strcmp( dir->d_name, "." ))
+			// fails at this step.
+				printf("%s\n", dir->d_name);
+			// printf("Test\n");
+		}
+		closedir(d);
+	}
 }
 
-char* 
+void command_processor()
+{
+		char buff[MAX];
+		printf("Enter the command :");
+		scanf("%s",buff);
+		printf("Command read Sucessfully\n");
+		if(strncmp("LSTU",buff,4) == 0){
+			printf("Entered LSTU command :- ");
+			LSTU();
+		}	
+}
+int main(){
+	command_processor();
+}
 
