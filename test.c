fc4f56e670e0c79bdbdfbd40304c4454150480de
kayalanithinsai <nithinkayala@gmail.com>
2021-03-05 11:57:21 +0530

    added socket connection but not working

--- test.c
+++ test.c
@@ -1,27 +1,14 @@
 #include <stdio.h> 
 #include <stdlib.h> // For exit() 
-
+#include<string.h>
 int main() 
 {
-	FILE *fptr2;
-	fptr2 = fopen("temp","w");
-	char c;
-	c = fgetc(fptr);
-	int cnt =0,n=0;
-	char messagebuffer[200];
-	while(cnt < 3){
-		if(c == '#'){
-			cnt++;
-		}
-		messagebuffer[n]=c;
-		n++;
-		if(cnt != 3){
-			c = fgetc(fptr);
-		}
-	}
-	mesasgebuffer[n] = '\0';
-	fputs(messagebuffer,fptr2);
-	fclose(fptr);
-	fclose(fptr2);
-	return 0; 
+	char buff[80] = "";
+	strcat(buff,"LSTU");
+	strcat(buff," ");
+	strcat(buff,"LSTM");
+	strcat(buff,"\n");
+	strcat(buff,"LSTM");
+	printf("%s",buff);
+	// puts(buff);
 }
